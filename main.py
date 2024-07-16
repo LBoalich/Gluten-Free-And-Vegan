840839978b9f9d35ff728aa34001c89b952d7c54
LBoalich <lkboalich@gmail.com>
2024-07-16 14:46:05 -0400

    Add DocString

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Name: Gluten Free and Vegan
+Author: Leah Boalich
+Date: 07/15/24
+Assignment: Final Project
+Short Description: This program allows users to search for restaurants in the Charlotte, NC region that serve gluten free options that are also vegan or vegetarian.  Users can also use a filter to find matching restaurants.  
+"""
