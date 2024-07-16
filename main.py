3dc708ee60f9acbfdc0c98ff4498a23745355414
LBoalich <lkboalich@gmail.com>
2024-07-16 15:30:56 -0400

    Add SearchWindow Class

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ Short Description: This program allows users to search for restaurants in the Ch
 import tkinter as tk
 from tkinter import ttk
 
+#Define search class
+class SearchWindow(ttk.Frame):
+    """"Allows user to search for restaurants."""
+    def __init__(self, parent):
+        """Sets up window and widgets."""
+        super().__init__(parent)
+        #Create widgets
+        self.searchLabel = ttk.Label(self, text="Restaurant Name:")
+        self.searchEntry = ttk.Entry(self)
+        self.searchButton = ttk.Button(self, text="Search")
+        #Add widgets to window
+        self.columnconfigure((0, 1, 2), weight=1, uniform=True)
+        self.searchLabel.grid(row=0, column=0)
+        self.searchEntry.grid(row=0, column=1)
+        self.searchButton.grid(row=0, column=2)
+
 #Define main class to run the program
 class Main(tk.Tk):
     """Allows users to search for restaurants using search or filter."""
@@ -17,7 +33,9 @@ class Main(tk.Tk):
         """Sets up the main window and widgets"""
         super().__init__()
         self.title("GLUTEN FREE AND VEGAN")
-        self.minsize(700, 400)
+        #Add widgets
+        self.search = SearchWindow(self)
+        self.search.pack()
         #Run the program
         self.mainloop()
 
