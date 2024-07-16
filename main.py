b6a3f60453efae3febea376c93f95916c2b0bc7d
LBoalich <lkboalich@gmail.com>
2024-07-16 14:49:03 -0400

    Add imports and Main Class

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ Date: 07/15/24
 Assignment: Final Project
 Short Description: This program allows users to search for restaurants in the Charlotte, NC region that serve gluten free options that are also vegan or vegetarian.  Users can also use a filter to find matching restaurants.  
 """
+
+#Imports
+import tkinter as tk
+from tkinter import ttk
+
+#Define main class to run the program
+class Main(tk.Tk):
+    """Allows users to search for restaurants using search or filter."""
+    def __init__(self):
+        """Sets up the main window and widgets"""
+        super().__init__()
+        self.title("GLUTEN FREE AND VEGAN")
+        self.minsize(700, 400)
+        #Run the program
+        self.mainloop()
+
+#Automatically run program
+if __name__ == "__main__":
+    Main()
