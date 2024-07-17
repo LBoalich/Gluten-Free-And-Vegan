760785756e71b3e2f9419e59423ba04a102e3a54
LBoalich <lkboalich@gmail.com>
2024-07-16 20:12:38 -0400

    Add ResultsWindowClass

--- main.py
+++ main.py
@@ -153,7 +153,7 @@ class PriceWindow(ttk.Frame):
         self.lowScale.grid(row=0, column=0, columnspan=2)
         self.highScale.grid(row=0, column=2, columnspan=2)
 
-#Define specials window
+#Define specials window class
 class SpecialsWindow(ttk.Frame):
     """Allows user to find restaurants that have happy hours or specails."""
     def __init__(self, parent):
@@ -164,6 +164,39 @@ class SpecialsWindow(ttk.Frame):
         self.specialsCheckbutton = tk.Checkbutton(self, text="Has Happy Hour or Specials", variable=self.specialCheck)
         self.specialsCheckbutton.grid(row=0, column=0)
 
+#Define filter results window class
+class ResultsWindow(ttk.Frame):
+    """Allows user to see results from filter and choose a restaurant to get more information"""
+    def __init__(self, parent):
+        """Sets up the window, widgets, and data."""
+        super().__init__(parent)
+        #Initializes the results data
+        self.resultsList = ["name1", "name2", "name3", "name4", "name5"]
+        #Create and add the label widget
+        self.resultsLabel = ttk.Label(self, text="Results")
+        self.resultsLabel.grid(row=0, column=0, columnspan=2)
+        #Create method to create and add the results
+        def addResults(self):
+            self.resultVar = tk.StringVar()
+            self.resultVar.set(self.resultsList[0])
+            row = 1
+            column = 0
+            for result in self.resultsList:
+                tk.Radiobutton(self, text=result, variable=self.resultVar, value=result).grid(row=row, column=column)
+                if column == 0:
+                    column += 1
+                else:
+                    row += 1
+                    column = 0  
+            return row     
+        self.lastRow = addResults(self)
+        #Create and add see more info button
+        self.infoButton = ttk.Button(self, text="See More Info", command=lambda :infoCommand(self))
+        self.infoButton.grid(row=(self.lastRow + 1), column=0, columnspan=2)
+        #Create command method for info button
+        def infoCommand(self):
+            return
+
 #Define main class to run the program
 class Main(tk.Tk):
     """Allows users to search for restaurants using search or filter."""
@@ -173,23 +206,25 @@ class Main(tk.Tk):
         self.title("GLUTEN FREE AND VEGAN")
         self.minsize(700, 400)
         #Create widgets
-        self.searchWindow = SearchWindow(self)
-        self.filterWindow = FilterWindow(self)
-        self.refineWindow = RefineWindow(self)
-        self.hourWindow = HourWindow(self)
-        self.categoryWindow = CategoryWindow(self)
-        self.neighborhoodWindow = NeighborhoodWindow(self)
-        self.priceWindow = PriceWindow(self)
-        self.specialsWindow = SpecialsWindow(self)
+        #self.searchWindow = SearchWindow(self)
+        #self.filterWindow = FilterWindow(self)
+        #self.refineWindow = RefineWindow(self)
+        #self.hourWindow = HourWindow(self)
+        #self.categoryWindow = CategoryWindow(self)
+        #self.neighborhoodWindow = NeighborhoodWindow(self)
+        #self.priceWindow = PriceWindow(self)
+        #self.specialsWindow = SpecialsWindow(self)
+        self.resultsWindow = ResultsWindow(self)
         #Add widgets
-        self.searchWindow.pack()
-        self.filterWindow.pack()
-        self.refineWindow.pack()
-        self.hourWindow.pack()
-        self.categoryWindow.pack()
-        self.neighborhoodWindow.pack()
-        self.priceWindow.pack()
-        self.specialsWindow.pack()
+        #self.searchWindow.pack()
+        #self.filterWindow.pack()
+        #self.refineWindow.pack()
+        #self.hourWindow.pack()
+        #self.categoryWindow.pack()
+        #self.neighborhoodWindow.pack()
+        #self.priceWindow.pack()
+        #self.specialsWindow.pack()
+        self.resultsWindow.pack()
         #Run the program
         self.mainloop()
 
